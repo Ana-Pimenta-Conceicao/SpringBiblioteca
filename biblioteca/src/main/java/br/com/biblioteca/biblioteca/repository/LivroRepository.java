2270b6de93e44d8abf15918d05d5a3fdc4cfd799
Ana-Pimenta-Conceicao <ana.pimenta120505@gmail.com>
2024-11-18 14:15:21 -0300

    :tada: Commit Inicial

---
+++ biblioteca/src/main/java/br/com/biblioteca/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,12 @@
+package br.com.biblioteca.biblioteca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.biblioteca.biblioteca.entity.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+    
+        
+}
+    
+
